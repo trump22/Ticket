3fa609c56f392d41dad045cc9ec09069d35df62d
Le Duc Giang <ducgiang20003@gmail.com>
2025-03-22 23:03:14 +0700

    Fix path to backend

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -45,9 +45,9 @@ const SearchBar = () => {
         <div className="w-full px-4 flex justify-center">
             <div ref={wrapperRef} className="relative w-full max-w-[376px]">
                 {/* Search Box */}
-                <div className="h-[47px] bg-white rounded-[13px] overflow-hidden w-full shadow-md">
+                <div className="h-[47px] bg-white rounded-xl overflow-hidden w-full shadow-md">
                     <div className="flex items-center w-full h-full px-2">
-                        <img src={searchIcon} alt="Search" className="w-6 h-6 md:w-8 md:h-8" />
+                        <img src={searchIcon} alt="Search" className="w-6 h-6 md:w-8 md:h-8"/>
                         <input
                             type="text"
                             placeholder="Bạn tìm gì hôm nay?"
@@ -56,18 +56,23 @@ const SearchBar = () => {
                             onFocus={() => setIsFocused(true)}
                             className="flex-1 outline-none border-none text-black/70 text-sm md:text-base font-normal font-['Inter'] px-2"
                         />
-                        <div className="hidden sm:block h-6 w-px bg-gray-300 mx-2" />
+                        <div className="hidden sm:block h-6 w-px bg-gray-300 mx-2 "/>
                         <button
-                            className="text-black text-sm md:text-base font-['Inter'] mr-1 md:mr-3 whitespace-nowrap"
+                            className="text-black text-sm md:text-base font-['Inter'] px-4 md:px-4 py-1 mr-1 md:mr-3
+                            whitespace-nowrap rounded-xl h-full
+
+                            bg-transparent hover:bg-gray-100 transition  "
                         >
                             Tìm kiếm
                         </button>
+
+
                     </div>
                 </div>
 
                 {/* Dropdown kết quả */}
                 {isFocused && filteredResults.length > 0 && (
-                    <ul className="absolute z-10 mt-2 w-full bg-white border border-gray-200 rounded-[10px] shadow-lg max-h-60 overflow-auto">
+                    <ul className="absolute z-10 mt-2 w-full bg-white border border-gray-200 rounded-xl shadow-lg max-h-60 overflow-auto">
                         {filteredResults.map((item, index) => (
                             <li
                                 key={index}
